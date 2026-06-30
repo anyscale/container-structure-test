350f99798163b752a9ece804adc112cfe64e345a
Thomas Desrosiers <git@hive.pw>
2026-06-29 22:08:25 -0700

    fix(vet): use keyed fields in EnvVar literal
    host_driver.go constructed unversioned.EnvVar with positional fields, the sole
    'go vet ./...' finding (composite literal uses unkeyed fields). Name the fields;
    go vet ./... now exits clean.

--- pkg/drivers/host_driver.go
+++ pkg/drivers/host_driver.go
@@ -87,7 +87,7 @@ func (d *HostDriver) SetEnv(envVars []unversioned.EnvVar) error {
 func SetEnvVars(envVars []unversioned.EnvVar) []unversioned.EnvVar {
 	var originalVars []unversioned.EnvVar
 	for _, envVar := range envVars {
-		originalVars = append(originalVars, unversioned.EnvVar{envVar.Key, os.Getenv(envVar.Key), envVar.IsRegex})
+		originalVars = append(originalVars, unversioned.EnvVar{Key: envVar.Key, Value: os.Getenv(envVar.Key), IsRegex: envVar.IsRegex})
 		if err := os.Setenv(envVar.Key, os.ExpandEnv(envVar.Value)); err != nil {
 			logrus.Errorf("Error setting env var: %s", err)
 		}
